70a19727c992d8d8fae14290c2b6a01528d61225
bost <bostsmiles@gmail.com>
2017-11-17 13:03:18 +0100

    balance module

---
+++ src/java/com/activedge/atm/balance/action/BalanceAction.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.activedge.atm.balance.action;
+
+import com.activedge.atm.web.common.BaseAction;
+
+/**
+ *
+ * @author postgres
+ */
+public class BalanceAction extends BaseAction{
+    
+}
---
+++ src/java/com/activedge/atm/balance/data/BalanceData.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.activedge.atm.balance.data;
+
+import com.activedge.atm.web.common.BaseData;
+import java.sql.Date;
+
+/**
+ *
+ * @author postgres
+ */
+public class BalanceData extends BaseData {
+    
+    private String invType;
+    private String invOrigin;
+    private String issuer;
+    private String terminalId;
+    private String currency;
+    private String inventoryRef;
+    private String tranDate;
+    private String tranTime;
+    private String totalPreAmount;
+    private String totalPostAmount;
+    private String preCount1;
+    private String postCount1;
+    private String preDenom1;
+    private String postDenom1;
+    private String preCount2;
+    private String postCount2;
+    private String preDenom2;
+    private String postDenom2;
+    private String preCount3;
+    private String postCount3;
+    private String preDenom3;
+    private String postDenom3;
+    private String preCount4;
+    private String postCount4;
+    private String preDenom4;
+    private String postDenom4;
+    private String preCount5;
+    private String postCount5;
+    private String preDenom5;
+    private String postDenom5;
+    private String preCount6;
+    private String postCount6;
+    private String preDenom6;
+    private String postDenom6;
+    private String preCount7;
+    private String postCount7;
+    private String preDenom7;
+    private String postDenom7;
+    private String preCount8;
+    private String postCount8;
+    private String preDenom8;
+    private String postDenom8;
+    private String preCount9;
+    private String postCount9;
+    private String preDenom9;
+    private String postDenom9;
+    private String preCount10;
+    private String postCount10;
+    private String preDenom10;
+    private String postDenom10;
+    private double authCostPost;
+    private double authPostpost;
+    private Date createDate;
+    private String status;
+    
+    
+}
